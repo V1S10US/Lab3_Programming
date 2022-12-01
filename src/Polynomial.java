298c200eecdf2d01d28a5cdd5f39e3f58731f184
Антон Меланин <a.g.melanin@gmail.com>
2022-12-01 12:18:36 +0300

    added symbol for choosing div/mod operation

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -20,6 +20,14 @@ public class Polynomial {
             monomes[i] = new Monom(p.monomes[i]);
     }
 
+    public boolean IsNull() {
+        for (Monom monom : monomes)
+            if (monom.a == 0)
+                return true;  // if at least one is null
+
+        return false;  // no zero coefficents
+    }
+
     public void Append(double coef, int x_degree, int sin_degree, int cos_degree) {
         monomes[num_monoms] = new Monom(coef, x_degree, sin_degree, cos_degree);
         num_monoms++;
@@ -40,16 +48,22 @@ public class Polynomial {
             System.out.println("Incorrect pos for deleting");
     }
 
-    public static Polynomial div(Polynomial M, Polynomial N) {
+    public static Polynomial div(Polynomial M, Polynomial N, char symbol) {
         boolean flag = true;
+
+        if (N.IsNull()) {
+            System.out.println("Invalid polynome");
+
+        }
+
         Polynomial div_polynomial = new Polynomial();
         int i = 1;
         int k = 1;
         int max = 0;
-        double coef_a;
-        int x_degree_a, sin_degree_a, cos_degree_a;
-        double coef_b;
-        int x_degree_b, sin_degree_b, cos_degree_b;
+        double coef_a = 0;
+        int x_degree_a = 0, sin_degree_a = 0, cos_degree_a = 0;
+        double coef_b = 0;
+        int x_degree_b = 0, sin_degree_b = 0, cos_degree_b = 0;
 
         while (flag) {
             for (Monom monom : M.monomes) {
@@ -136,10 +150,16 @@ public class Polynomial {
             if (div_polynomial.num_monoms == 0)
                 System.out.println("Unable to divide ");
         }
+        if (symbol == '/')
+            return div_polynomial;
+        else if (symbol == '%')
+            return M;
+        return div_polynomial;
     }
 
-
+/*
     public static Polynomial mod(Polynomial a, Polynomial b) {
         Polynomial mod_polynomial = new Polynomial();
     }
 }
+*/
